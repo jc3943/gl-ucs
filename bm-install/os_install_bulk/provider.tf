696229859d98a97e59faccdcfd601a2f9bc5d280
jeff.comer <jeff.comer@gitlab.com>
2026-02-24 11:53:36 -0500

    added hack vault replication that will need revised for more complete solution

--- bm-install/os_install_bulk/provider.tf
+++ bm-install/os_install_bulk/provider.tf
@@ -1,3 +1,8 @@
+#Authored: Jeff Comer
+# data "vault_generic_secret" "intersight" {
+#   path = "intersight/intersight_api"
+# }
+
 terraform {
   #required_version = ">= 1.3.6"
   required_providers {
@@ -8,8 +13,13 @@ terraform {
   }
 }
 
+#provider "vault" {}
+
 provider "intersight" {
   apikey    = "6457bfa47564612d300f0917/6457cbbd7564612d30cb32ab/64595f8c7564612d30cb47cc"
   secretkey = "../../creds/dev-isight-SecretKey.txt"
   endpoint  = "https://dev-intersight.thor.iws.navy.mil"
+  # apikey = data.vault_generic_secret.intersight.data["api_key_id"]
+  # secretkey = data.vault_generic_secret.intersight.data["secret_key_string"]
+  # endpoint = data.vault_generic_secret.intersight.data["target-url"]
 }
